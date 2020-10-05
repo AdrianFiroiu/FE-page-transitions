e78688e460288660a2f2e9c0fcba5dca01917ca0
Mihai F <mihai.a.firoiu@gmail.com>
2020-10-05 23:49:36 +0300

    Resize imgs, fix template, fix js

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ function pageTransition() {
     
     var tl = gsap.timeline();
 
-    tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: 'bottom left', stagger: .2})
-    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: 'bottom left', stagger: .1, delay:.1})
+    tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
 }
 
 function contentAnimation() {
@@ -14,7 +14,7 @@ function contentAnimation() {
     var tl = gsap.timeline;
 
     tl.from('.left', {duration: 1.5, translateY: 50, opacity: 0})
-    tl.to('img', {clipPath:"polygon(0 0, 100% 0, 100% 0, 0 0)"}, "-=1.1")
+    tl.to('img', {clipPath:"polygon(0 0, 100% 0, 100% 100%, 0 100%)"}, "-=1.1")
 }
 
 function delay(n) {
